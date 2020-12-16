f4dab74c30ad6a10f5800e67f6d47f729b1cecd7
hollywo <aabedalaabed9@gmail.com>
2020-12-16 20:56:33 +0200

    Main Methode Contains two statuss

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include"imp.h"
+#include<Windows.h>
+int main(){
+	
+	Stack<int>a;
+
+	//isEmpty() returns bool value if size == 0
+	//push() add value to stack and returns bool value if it runs as exepicted or not 
+
+	/*cout<<a.isEmpty();
+	cout<<endl<<a.Push(1);////////////////////////////////           first status
+	cout<<endl<<a.isEmpty()<<endl;
+	*/
+	
+
+	//cout<<a.isEmpty()<<endl<<a.Push(1)<<endl<<a.isEmpty()<<endl;       //secound status
+
+system("pause");
+}
+
+
+	
+	
+	
+	
+
+
+
+
+
+
